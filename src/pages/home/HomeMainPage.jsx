ba4bd3aea1f2b7e55f0a6e6cfd5efb55188dcf8a
songdaramji <songdaramji@gmail.com>
2024-11-18 18:38:17 +0900

    feat(homeSearch

--- src/pages/home/HomeMainPage.jsx
+++ src/pages/home/HomeMainPage.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import BasicLayout from "../../layouts/BasicLayout";
-import { OnlyTitle, PrevAddBook } from "../../layouts/TopLayout";
+import { TitleSearch } from "../../layouts/TopLayout";
 // 책 추천 홈 페이지
 const HomePage = () => {
   return (
-    <>
-      <PrevAddBook title={"책 전체 검색"}></PrevAddBook>
-      <BasicLayout />
-    </>
+    <BasicLayout>
+      <TitleSearch title={"홈"} showLine={false} />
+    </BasicLayout>
   );
 };
 
